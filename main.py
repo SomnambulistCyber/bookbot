6ed1d858224740cceba1ea036f790e4839a47f72
SomnambulistCyber <70433163+SomnambulistCyber@users.noreply.github.com>
2025-03-18 16:03:05 +0000

    quick refactor and modularisation

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
+from stats import get_num_words
+
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
     return file_contents
 
-def split_words(file_contents):
-    words = file_contents.split()
-    return words
-
 def main():
     path = "books/frankenstein.txt"
-    all_words = split_words(get_book_text(path))
-    num_words = len(all_words)
+    num_words = get_num_words(get_book_text(path))
     print(f"{num_words} words found in the document")
 
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(file_contents):
+    all_words = file_contents.split()
+    num_words = len(all_words)
+    return num_words
