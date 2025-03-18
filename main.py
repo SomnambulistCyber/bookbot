2092b9f20d88b62be415144ac50ecb5494071d97
SomnambulistCyber <70433163+SomnambulistCyber@users.noreply.github.com>
2025-03-18 16:25:54 +0000

    count all characters in the book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_num_char
 
 def get_book_text(path):
     with open(path) as f:
@@ -9,5 +10,7 @@ def main():
     path = "books/frankenstein.txt"
     num_words = get_num_words(get_book_text(path))
     print(f"{num_words} words found in the document")
+    num_chars = get_num_char(get_book_text(path))
+    print(f"{num_chars} words found in the document")
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,13 @@
+char_count = {}
+
 def get_num_words(file_contents):
     all_words = file_contents.split()
     num_words = len(all_words)
     return num_words
+
+def get_num_char(file_contents):
+    all_lower = str.lower(file_contents)
+    all_char = list(all_lower)
+    for letter in list(map(chr,range(ord('a'),ord('z')+1))):
+        char_count[letter] = all_char.count(letter)
+    return char_count
