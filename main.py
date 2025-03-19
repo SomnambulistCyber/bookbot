d2a79c920e40c351361dfd36db83f04e70fe1b64
SomnambulistCyber <70433163+SomnambulistCyber@users.noreply.github.com>
2025-03-19 14:45:09 +0000

    Add exception and command line arguements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_words
 from stats import get_num_char
 from stats import sort_func
+import sys
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,16 +9,19 @@ def get_book_text(path):
     return file_contents
 
 def main():
-    path = "books/frankenstein.txt"
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {path}")
-    print("----------- Word Count ----------")
-    num_words = get_num_words(get_book_text(path))
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    sorted_chars = sort_func(get_book_text(path))
-    for entry in sorted_chars:
-        print(f"{entry['letter']}: {entry['num']}")
-    print("============= END ===============")
-
+    args = sys.argv
+    try:
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {args[1]}")
+        print("----------- Word Count ----------")
+        num_words = get_num_words(get_book_text(args[1]))
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        sorted_chars = sort_func(get_book_text(args[1]))
+        for entry in sorted_chars:
+            print(f"{entry['letter']}: {entry['num']}")
+        print("============= END ===============")
+    except Exception as e:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 main()
