13d22c792cf7799986ea17f1831eec4e84e1336f
SomnambulistCyber <70433163+SomnambulistCyber@users.noreply.github.com>
2025-03-19 11:59:47 +0000

    sorted dictionary and fixed formatting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_num_char
+from stats import sort_func
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,9 +9,15 @@ def get_book_text(path):
 
 def main():
     path = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}")
+    print("----------- Word Count ----------")
     num_words = get_num_words(get_book_text(path))
-    print(f"{num_words} words found in the document")
-    num_chars = get_num_char(get_book_text(path))
-    print(f"{num_chars} words found in the document")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    sorted_chars = sort_func(get_book_text(path))
+    for entry in sorted_chars:
+        print(f"{entry['letter']}: {entry['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,13 @@
 char_count = {}
+char_list = []
 
 def get_num_words(file_contents):
     all_words = file_contents.split()
     num_words = len(all_words)
     return num_words
+
+def sort_on(dict):
+    return dict["num"]
 
 def get_num_char(file_contents):
     all_lower = str.lower(file_contents)
@@ -11,3 +15,17 @@ def get_num_char(file_contents):
     for letter in list(map(chr,range(ord('a'),ord('z')+1))):
         char_count[letter] = all_char.count(letter)
     return char_count
+
+def sort_func(file_contents):
+    all_lower = str.lower(file_contents)
+    all_char = list(all_lower)
+    for letter in list(map(chr,range(ord('a'),ord('z')+1))):
+        letter_dict = {
+            "letter": letter,
+            "num": all_char.count(letter)
+        }
+        if letter.isalpha():
+            char_list.append(letter_dict)
+            char_list.sort(reverse=True, key=sort_on)
+    return char_list
+
